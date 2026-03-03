2d920dd8fc95e5a2e590f5ebd19806d5c4fe4100
pnackp <pnackp@gmail.com>
2026-03-04 01:33:32 +0700

    frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './login/login';
-import { RegisterComponent } from './register/register';
-import { Dashboard } from './dashboard/dashboard';
+import { Login } from './login/login';
+import { Register } from './register/register';
 
 export const routes: Routes = [
-    {path : '' , pathMatch:'full' , redirectTo: 'login'},
-    {path : 'login' , component:LoginComponent , title:'TrackSystem • Login'},
-    {path : 'register' , component:RegisterComponent , title: 'TrackSystem • Register'},
-    {path : 'dashboard' , component:Dashboard , title: 'TrackSystem • Dashboard'}
+    {path: '' , redirectTo: "login" , pathMatch:'full'},
+    {path: 'login' , component : Login},
+    {path: 'register' , component : Register}
 ];
